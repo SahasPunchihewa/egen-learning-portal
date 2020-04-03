dafa398227ddbbf8f463b8b0abe32ea0af6071bd
kushanshamika <shamikakushan@gmail.com>
2020-04-03 18:55:25 +0530

    Add file upload portal

---
+++ scripts/portal.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Unicat</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Unicat project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/main_styles_status.css">
+<link rel="stylesheet" type="text/css" href="../styles/responsive.css">
+</head>
+<body>
+
+<div class="super_container">
+
+	<header class="header">
+			
+		<!-- Top Bar -->
+		<div class="top_bar">
+			<div class="top_bar_container">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<div class="top_bar_content d-flex flex-row align-items-center justify-content-start">
+								<ul class="top_bar_contact_list">
+									<li><div class="question">Have any questions?</div></li>
+									<li>
+										<i class="fa fa-phone" aria-hidden="true"></i>
+										<div>+94 777 342 065</div>
+									</li>
+									<li>
+										<i class="fa fa-envelope-o" aria-hidden="true"></i>
+										<div>prasannagalle@gmail.com</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>				
+		</div>
+
+		<!-- Header Content -->
+		<div class="header_container">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="header_content d-flex flex-row align-items-center justify-content-start">
+							<div class="logo_container">
+								<a href="#">
+									<div class="logo_text"><img src="../images/icon.png"/>&ensp;INSTITUTE OF ICT</div>
+								</a>
+							</div>
+							<nav class="main_nav_contaner ml-auto">
+								<ul class="main_nav">
+									<li><a href="index.php">Home</a></li>
+									<li><a href="downloads.php">Downloads</a></li>
+									<li><a href="login.php">Staff Login</a></li>
+									<li class="active"><a href="status.php">Status</a></li>
+								</ul>
+								
+
+								<!-- Hamburger -->
+
+								
+								<div class="hamburger menu_mm">
+									<i class="fa fa-bars menu_mm" aria-hidden="true"></i>
+								</div>
+							</nav>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+	</header>
+
+	<!-- Menu -->
+
+	<div class="menu d-flex flex-column align-items-end justify-content-start text-right menu_mm trans_400">
+		<div class="menu_close_container"><div class="menu_close"><div></div><div></div></div></div>
+		<nav class="menu_nav">
+			<ul class="menu_mm">
+				<li class="menu_mm"><a href="index.php">Home</a></li>
+				<li class="menu_mm"><a href="#">Staff Portal</a></li>
+			</ul>
+		</nav>
+	</div>
+	
+	<!-- Home -->
+
+	<div class="home">
+		<div class="breadcrumbs_container">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="breadcrumbs">
+							<ul>
+								<li><a href="index.php">Home</a></li>
+								<li><a href="login.php">Staff Login</a></li>
+								<li>Staff Portal</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>			
+	</div>
+
+    <?php
+
+include("db.php");
+
+$error_count = 0;
+
+$sql = "Insert INTO doc(docTitle, docGrade, docType, docLink, docDate) VALUES('".$_POST['title']."',".$_POST['grade'].",'".$_POST['filetype']."','".$_POST['link']."','".date("Y/m/d")."')";
+
+if (mysqli_query($conn, $sql)) {
+    $last_id = mysqli_insert_id($conn);
+    //echo "New record created successfully. Last inserted ID is: " . $last_id;
+} else {
+    $error_count += 1;
+}
+
+
+if($_POST['filetype'] == "paper" or $_POST['filetype'] == "paper"){
+    if(isset($_FILES['document'])){
+        $errors= array();
+        $file_size = $_FILES['document']['size'];
+        $file_tmp = $_FILES['document']['tmp_name'];
+        $file_type = $_FILES['document']['type'];
+        $file_ext=strtolower(end(explode('.',$_FILES['document']['name'])));
+        $file_name = $last_id.".".$file_ext;
+        
+        $extensions= array("pdf");
+        
+        if(in_array($file_ext,$extensions)=== false){
+           $errors[]="Extension not allowed, please choose PDF file.";
+        }
+        
+        if(empty($errors)==true) {
+           move_uploaded_file($file_tmp,"../uploads/".$file_name);
+           //echo "Success";
+        }else{
+          $error_count += 1;
+        }
+     }
+}
+
+
+   if($error_count >0){
+    $msg = "File upload failed";
+   }else{
+    $msg = "Your file has been uploaded successfully.";
+   }
+?>
+
+	<!-- Features -->
+
+	<div class="features">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="section_title_container text-center">
+						<h2 class="section_title">
+						</h2>
+						<div class="about">
+							<div class="container">
+								<div class="row">
+									<div class="col">
+										<div class="section_title_container text-center">
+											<h2 class="section_title"><?php echo($msg)?></h2>
+											<div class="section_subtitle"><p>Click <a href="../portal.php">here</a>  to access the file upload portal</p>
+											</div>
+										</div>
+									</div>
+								</div>
+     						</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+
+	<!-- Footer -->
+
+	<footer class="footer">
+		
+		<div class="container">
+			<div class="row footer_row">
+				<div class="col">
+					<div class="footer_content">
+						<div class="row">
+
+							<div class="col-lg-3 footer_col">
+					<!-- Footer About -->
+								<div class="footer_section footer_about">
+									<div class="footer_logo_container">
+										<a href="#">
+										<div class="footer_logo_text">E<span>GEN</span></div>
+										</a>
+									</div>	
+									<div class="cr_text">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/custom.js"></script>
+</body>
+</html>
